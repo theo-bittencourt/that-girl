8c652c5849c8202a999407fff4982e225fe10099
Theo Bittencourt <theodrums@gmail.com>
2012-10-07 00:54:36 -0300

    revert last

--- app/controllers/pages_controller.rb
+++
@@ -1,6 +0,0 @@
-class PagesController < ApplicationController
-  
-  def about_us
-  end
-  
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 ThatGirl::Application.routes.draw do
-  root :to => "pages#about_us"
-  
-  match 'about_us' => 'pages#about_us', :as => :about_us
-  
+  root :to => "home#index"
 end
