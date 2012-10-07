ec97d999b73558a6c812d3c8845657e7d8c2a8f6
Theo Bittencourt <theodrums@gmail.com>
2012-10-07 01:04:57 -0300

    protect_from_forgery :except => [:about_us_post]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  protect_from_forgery :except => [:about_us_post]
+  
+  def about_us_post
+    redirect_to :about_us
+  end
   
   def about_us
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ThatGirl::Application.routes.draw do
   root :to => "pages#about_us"
   
+  post 'about_us' => 'pages#about_us_post'
+  
   match 'about_us' => 'pages#about_us', :as => :about_us
   
 end
